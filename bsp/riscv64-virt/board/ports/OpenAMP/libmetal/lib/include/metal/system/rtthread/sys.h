b8879c40d8395be8dde26b400f3c365f257a7de7
Bamvor Jian ZHANG <bamv2005@gmail.com>
2020-11-02 15:48:19 +0800

    libmetal: headers
    Signed-off-by: Bamvor Jian ZHANG <bamv2005@gmail.com>

---
+++ bsp/riscv64-virt/board/ports/OpenAMP/libmetal/lib/include/metal/system/rtthread/sys.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2016, Xilinx Inc. and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/*
+ * @file	rtthread/sys.h
+ * @brief	FreeRTOS system primitives for libmetal.
+ */
+
+#ifndef __METAL_SYS__H__
+#error "Include metal/sys.h instead of metal/rtthread/sys.h"
+#endif
+
+#ifndef __METAL_RTTHREAD_SYS__H__
+#define __METAL_RTTHREAD_SYS__H__
+
+//#include "./@PROJECT_MACHINE@/sys.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __METAL_RTTHREAD_SYS__H__ */
