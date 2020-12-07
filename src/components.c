8a02952b9d27463e018402451033683add381ca3
Bamvor Jian ZHANG <bamv2005@gmail.com>
2020-12-07 15:17:59 +0800

    enable create main_thread_entry for debugging schedule issue
    For issue #20
    
    Signed-off-by: Bamvor Jian ZHANG <bamv2005@gmail.com>

--- src/components.c
+++ src/components.c
@@ -195,7 +195,6 @@ void rt_application_init(void)
     rt_thread_t tid;
 
     main();
-#if 0
 #ifdef RT_USING_HEAP
     tid = rt_thread_create("main", main_thread_entry, RT_NULL,
                            RT_MAIN_THREAD_STACK_SIZE, RT_MAIN_THREAD_PRIORITY, 20);
@@ -210,7 +209,6 @@ void rt_application_init(void)
 
     /* if not define RT_USING_HEAP, using to eliminate the warning */
     (void)result;
-#endif
 #endif
 
     rt_thread_startup(tid);
