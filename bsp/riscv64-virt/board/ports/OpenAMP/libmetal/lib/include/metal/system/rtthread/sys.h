857ab0aaa3f588b992d30cbd4849d0a4b31af2d5
Bamvor ZHANG <bamv2005@gmail.com>
2021-05-11 21:21:38 +0800

    libmetal: irq
    Signed-off-by: Bamvor ZHANG <bamv2005@gmail.com>

--- bsp/riscv64-virt/board/ports/OpenAMP/libmetal/lib/include/metal/system/rtthread/sys.h
+++ bsp/riscv64-virt/board/ports/OpenAMP/libmetal/lib/include/metal/system/rtthread/sys.h
@@ -1,36 +1,55 @@
 /*
- * Copyright (c) 2016, Xilinx Inc. and Contributors. All rights reserved.
+ * Copyright (c) 2015, Xilinx Inc. and Contributors. All rights reserved.
+ * Copyright (c) 2021, Bamvor Jian Zhang. RTT. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
 /*
- * @file	rtthread/sys.h
- * @brief	FreeRTOS system primitives for libmetal.
+ * @file	rtthread/riscv/sys.h
+ * @brief	rtthread riscv system primitives for libmetal.
  */
 
 #ifndef __METAL_SYS__H__
-#error "Include metal/sys.h instead of metal/rtthread/sys.h"
+#error "Include metal/sys.h instead of metal/rtthread/@PROJECT_MACHINE@/sys.h"
 #endif
 
-#ifndef __METAL_RTTHREAD_SYS__H__
-#define __METAL_RTTHREAD_SYS__H__
+#ifndef __METAL_RTTHREAD_RISCV_SYS__H__
+#define __METAL_RTTHREAD_RISCV_SYS__H__
 
-//#include "./@PROJECT_MACHINE@/sys.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-/** Structure for rt-thread libmetal runtime state. */
+/** Structure of generic libmetal runtime state. */
 struct metal_state {
 
 	/** Common (system independent) data. */
 	struct metal_common_state common;
 };
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+/**
+ * @brief	metal_rv64_virt_irq_isr
+ *
+ * riscv64 virt interrupt ISR can be registered to the Xilinx embeddedsw
+ * IRQ controller driver.
+ *
+ * @param[in] arg input argument, interrupt vector id.
+ */
+void metal_rv64_virt_irq_isr(void *arg);
+
+/**
+ * @brief	metal_rv64_virt_irq_int
+ *
+ * riscv64 virt interrupt controller initialization. It will initialize
+ * the metal Xilinx IRQ controller data structure.
+ *
+ * @return 0 for success, or negative value for failure
+ */
+int metal_rv64_virt_irq_init(void);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* __METAL_RTTHREAD_SYS__H__ */
+#endif /* __METAL_RTTHREAD_RISCV_SYS__H__ */
